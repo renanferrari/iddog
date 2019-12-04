ba68d77a69e04de5ae8db006de9183e4ff5d5dad
Renan Ferrari <renan.fb@gmail.com>
2019-12-03 23:01:27 -0300

    Adding login screen

--- app/src/main/java/com/renanferrari/iddog/login/LoginActivity.kt
+++ app/src/main/java/com/renanferrari/iddog/login/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.renanferrari.iddog
+package com.renanferrari.iddog.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.renanferrari.iddog.R.layout
 
 class LoginActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(layout.activity_login)
   }
 }
